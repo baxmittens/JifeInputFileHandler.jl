a19e1a15f6e757a89b75ab35652e7364a8c9657d
Max Bittens <maximilian.bittens@bgr.de>
2024-12-03 16:06:19 +0100

    Update JifeInputFileHandler.jl

--- src/JifeInputFileHandler.jl
+++ src/JifeInputFileHandler.jl
@@ -11,6 +11,4 @@ end
 include(joinpath(".","JifeInputFileHandler","io.jl"))
 include(joinpath(".","JifeInputFileHandler","utils.jl"))
 
-export JifeModelDef, getAllPathesbyAttribute, rename!, getElementbyPath,  format_ogs_path, setAttributeValbyPath!, getAllPathesbyAttribute!
-
 end # module
